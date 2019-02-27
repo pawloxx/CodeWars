955947cd07562cb0e9381ea19d9458e44c661140
pawloxx <murinus.dot@gmail.com>
2019-02-27 15:33:38 +0100

    description added

--- CreditCardMask.py
+++ CreditCardMask.py
@@ -1,6 +1,10 @@
+"""
+Your task is to write a function maskify, which changes all but the last four characters into '#'
+"""
+
 # return masked string
 #input
-cc = 'asdasdasdasdsad'
+cc = 'input'
 #masking function
 def maskify(cc):
     mask = '#'*(len(cc) - 4) + cc[-4:]
