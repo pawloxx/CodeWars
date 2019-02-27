3afd415e06caf021e47f17b84ed7093ac500cb05
pawloxx <murinus.dot@gmail.com>
2019-02-27 14:51:28 +0100

    changes: int to string replace in to range add some tests from codewars

--- CreditCardValidifier.py
+++ CreditCardValidifier.py
@@ -1,21 +1,37 @@
-num = '1111'
+#num = '51'
 def credit(num):
-    val = len(num)
-    li = ['VISA', 'MasterCard', 'AMEX', 'Discover', 'Invalid']
+    number_as_string = str(num)
+    number_length = len(number_as_string)
+    providers_list = ['VISA', 'MasterCard', 'AMEX', 'Discover', 'Invalid']
+    #print(number_as_string[0])
+    print(number_length)
+    #print(number_as_string[]) in (51,55)
+    #print((number_length == 13 or number_length == 16))
+    #print(int("55") in (51, 55))
+    #print(number_as_string[:2])
+    #print('37' in (34, 37))
 #VISA
-    if num[:1] == '4' and (val == 13 or val == 16):
-        return li[0]
+    if (number_as_string[0] == '4' and (number_length == 13 or number_length == 16)):
+        return providers_list[0]
 #MasterCard
-    elif int(num[2]) in (51,55) and val == 16:
-        return li[1]
+    elif int(number_as_string[:2]) in range(51, 56) and number_length == 16:
+        return providers_list[1]
 #AMEX
-    elif (num[:2] == '34' or num[:2] == '37') and val == 16:
-        return li[2]
+    elif int(number_as_string[:2]) in (34, 37) and number_length == 15:
+        return providers_list[2]
 #Discover
-    elif num[:4] == '6011' and val == 16:
-        return li[3]
+    elif number_as_string[:4] == '6011' and number_length == 16:
+        return providers_list[3]
 #Invalid
     else:
-        return li[4]
+        return providers_list[4]
 
-print(credit(num))
+print(credit(4128374839283))
+
+"""
+Test.assert_equals(credit(6011364837263748), "Discover")
+Test.assert_equals(credit(5318273647283745), "MasterCard")
+Test.assert_equals(credit(12345678910), "Invalid")
+Test.assert_equals(credit(371236473823676), "AMEX")
+Test.assert_equals(credit(4128374839283), "VISA")
+"""
