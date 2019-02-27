411a16a54a7364cc70a48b62978c5aa84a0f47cc
pawloxx <murinus.dot@gmail.com>
2019-02-27 15:29:10 +0100

    comments and tests deleted

--- CreditCardValidifier.py
+++ CreditCardValidifier.py
@@ -1,15 +1,19 @@
-#num = '51'
+"""
+Make a program that sees if a credit card number is valid or not.
+Also the program should tell you what type of credit card it is if it is valid.
+The five things you should consider in your program is: AMEX, Discover, VISA, Master, and Invalid :
+Discover starts with 6011 and has 16 digits,
+AMEX starts with 34 or 37 and has 15 digits,
+Master Card starts with 51-55 and has 16 digits,
+VISA starts with 4 and has 13 or 16 digits.
+
+
+"""
 def credit(num):
     number_as_string = str(num)
     number_length = len(number_as_string)
     providers_list = ['VISA', 'MasterCard', 'AMEX', 'Discover', 'Invalid']
-    #print(number_as_string[0])
-    #print(number_length)
-    #print(number_as_string[]) in (51,55)
-    #print((number_length == 13 or number_length == 16))
-    #print(int("55") in (51, 55))
-    #print(number_as_string[:2])
-    #print('37' in (34, 37))
+
 #VISA
     if (number_as_string[0] == '4' and (number_length == 13 or number_length == 16)):
         return providers_list[0]
@@ -25,13 +29,10 @@ def credit(num):
 #Invalid
     else:
         return providers_list[4]
-
-#print(credit(4128374839283))
-
 """
-Test.assert_equals(credit(6011364837263748), "Discover")
-Test.assert_equals(credit(5318273647283745), "MasterCard")
-Test.assert_equals(credit(12345678910), "Invalid")
-Test.assert_equals(credit(371236473823676), "AMEX")
-Test.assert_equals(credit(4128374839283), "VISA")
+assert(credit(6011364837263748), "Discover")
+assert(credit(5318273647283745), "MasterCard")
+assert(credit(12345678910), "Invalid")
+assert(credit(371236473823676), "AMEX")
+assert(credit(4128374839283), "VISA")
 """
