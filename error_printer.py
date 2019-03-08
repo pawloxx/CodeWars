810f70e5dfbbf5e931e9588e6c28400f98817290
pawloxx <murinus.dot@gmail.com>
2019-03-08 14:05:20 +0100

    1st use of format and variables rename

--- error_printer.py
+++ error_printer.py
@@ -6,8 +6,9 @@
 # error_printer(s) => "8/22"
 
 
-def printer_error(s):
-    return str(len(['' + i for i in s if i not in "abcdefghijklm"])) + "/" + str(len(s))
+def printer_error(input_string):
+    length_of_chars_not_in_string = len(['' + i for i in input_string if i not in "abcdefghijklm"])
+    return "{0}/{1}".format(length_of_chars_not_in_string, (len(input_string)))
 
 
 print(printer_error("aaabbbbhaijjjmxxxx"))
